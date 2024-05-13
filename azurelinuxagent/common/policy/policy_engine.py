19bbfd7cfb8dc4c1a56665b257551d22c85b61e9
mgunnala <manugunnala@microsoft.com>
2024-05-13 15:35:05 -0700

    Changes

--- azurelinuxagent/common/policy/policy_engine.py
+++ azurelinuxagent/common/policy/policy_engine.py
@@ -23,5 +23,68 @@ import regorus
 
 class PolicyEngine:
     """Base class for policy engine"""
-    def __init__(self):
+    def __init__(self, policy_file=None, data_file=None):
         self._engine = regorus.Engine()
+        if policy_file is not None:
+            self._engine.add_policy_from_file(policy_file)
+        if data_file is not None:
+            self.add_data(data_file)
+
+
+"""
+class PolicyEngine::Singleton:  
+
+policy_path: str 
+
+data_path: str 
+
+def __init__(policy_file: path, data_file: path) 
+
+def add_policy_rule(policy_file: path) 
+
+def add_data(data: json or path) 
+
+def set_input(input: json or path) 
+
+def eval_query(query: str) -> return json 
+
+def update_data( ) 
+
+def __clear_data( ) 
+
+  
+
+class ExtensionPolicyEngine(PolicyEngine):  
+
+allowed_list: list = none  
+
+all_extensions: list = none 
+
+require_signed : bool 
+
+def get_global_policy() -> return json 
+
+def get_allowed_list(all_extensions: list) -> return list 
+
+def get_policy_for_extension(extension, enforcement_point=none) -> return json 
+
+def is_extension_allowed(extension_name: str) -> return bool 
+
+def is_extension_download_allowed(extension_name: str) -> return bool 
+
+def is_extension_install_allowed(extension_name: str) -> return bool 
+
+def is_extension_update_allowed(extension_name: str) -> return bool 
+
+def is_extension_uninstall_allowed(extension_name: str) -> return bool 
+
+def is_valid_signature(signature, signing_method) -> return bool 
+
+def __convert_list_to_json(all_extensions: list) -> return regorus-compatible json 
+
+  
+
+class GoalStatePolicyEngine(PolicyEngine): 
+
+def is_valid_signature() -> return bool 
+"""
